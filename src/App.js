596dae8768aaa510c2e3f9b6ad7dca9d5f2e1101
Saif Kbishi <Saifkbishi@gmail.com>
2021-04-01 16:57:47 +0300

    netlify

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    document.title="FlexTickets",
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site still under creation
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Buy a Ticket
         </a>
       </header>
     </div>
