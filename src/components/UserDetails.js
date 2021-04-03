9095faa7ceb503b5b43ca1c55e5abcabafe74219
Saif Kbishi <Saifkbishi@gmail.com>
2021-04-03 23:59:39 +0300

    user details

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -3,28 +3,34 @@ import { useForm } from 'react-hook-form';
 import '../css/tickets.css';
 
 const UserDetails = () => {
- const {register, handleSubmit, errors} = useForm();
+ const {register, handleSubmit, formState: { errors }} = useForm();
 
  const onSubmit =(data)=>{
   console.log(data)
  }
   return (
    document.title="FlexTickets - UserDetails",
-   <div className="App" >     
+   <div className="UserDetails" >     
     hello from UserDetails
-   <form onSubmit={handleSubmit(onSubmit)}>
-    <input type="text" placeholder="First name" {...register("firstName", { required: true, minLength:2, maxLength: 20 })}/><br/>
-    <input type="text" placeholder="Last name" name="lastname" {...register("lastname", { required: true, minLength:2, maxLength: 20 })}/><br/>
-    <input type="text" placeholder="E-mail" name="email" {...register("email", { required: true})}/><br/>
-    <input type="number" placeholder="Cell Phone" name="cellphone" {...register("cellphone", { required: true})}/><br/>
-    <input type="text" placeholder="Birthdate" name="birthdate" {...register("birthdate", { required: true})}/><br/>
-    <input type="text" placeholder="" name=""></input>
+   <form onSubmit={handleSubmit(onSubmit)} className="registerForm">
+    <input type="text" placeholder="First name" {...register("firstName", { required: true, minLength:2, maxLength: 20 })}/>
+    {errors.firstName && "First name is required"}<br/>
+    <input type="text" placeholder="Last name" name="lastname" {...register("lastname", { required: true, minLength:2, maxLength: 20 })}/>
+    {errors.lastname && "Last name is required"}<br/>
+    <input type="text" placeholder="E-mail" name="email" {...register("email", { required: true})}/>
+    {errors.email && "E-mail is required to send you confirmation"}<br/>
+    <input type="number" placeholder="Cell Phone" name="cellphone" {...register("cellphone", { required: true})}/>
+    {errors.cellphone && "Cell Phone is required"}<br/>
+    <label for="birthday">Birthday:  </label>
+    <input type="date" placeholder="Birthday" name="birthday" {...register("birthday", { required: true})}/>
+    {errors.birthday && "Birthday is required"}<br/>    
     <hr/>
-    <input type="number" placeholder="ID" name="ID"/><br/>
-    <input type="text" placeholder="CC number" name="CC"/><br/>
-    <input type="text" placeholder="CVV" name="cvv"/>
-    <input type="text" placeholder="Exp. Date" name="epxDate"/>
-    <input type="text" placeholder="Calendar" name=""/>
+    <input type="number" placeholder="ID" name="ID" {...register("id", { required: true})}/><br/>
+    <input type="text" placeholder="CC number" name="CC" {...register("CC number", { required: true})}/><br/>
+    <input type="text" placeholder="CVV" name="cvv" {...register("CVV", { required: true})}/><br/>
+    <label for="epxDate">Expiry Date: </label>
+    <input type="date" placeholder="Exp. Date" name="epxDate" {...register("epxDate", { required: true})}/>
+    
     <br/><br/>
     <input type="submit"/>
    </form>
